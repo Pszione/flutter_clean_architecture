49c5cb8d0245e4da3e81097fe23f894bc350d11c
Pszione <oused.games@gmail.com>
2021-11-26 00:03:01 -0300

    [05] CLEAN ARCHITECTURE: TDD NA PRÁTICA | GetCarsByColorRepository

--- test/layers/data/repositories/get_cars_by_color_repository_test.dart
+++ test/layers/data/repositories/get_cars_by_color_repository_test.dart
@@ -1,25 +1,10 @@
-import 'package:flutter_clean_architecture/layers/data/dtos/car_dto.dart';
-import 'package:flutter_clean_architecture/layers/domain/entities/car_entity.dart';
+import 'package:flutter_clean_architecture/layers/data/repositories/get_cars_by_color_repository_imp.dart';
 import 'package:flutter_clean_architecture/layers/domain/repositories/get_cars_by_color_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-class GetCarsByColorRepositoryImp implements GetCarsByColorRepository {
-  @override
-  CarEntity call(String color) {
-    // We called the API and it returned a map/JSON
-    var json = {
-      "plate": 'ABC',
-      "factoryColor": 'any color',
-      "doors": 3,
-      "factoryPrice": 50000.0
-    };
-    return CarDto.fromMap(json);
-  }
-}
-
 void main() {
   //
-  GetCarsByColorRepository? repository = GetCarsByColorRepositoryImp();
+  GetCarsByColorRepository repository = GetCarsByColorRepositoryImp();
 
   test('Should receive a car entity no matter the color', () {
     // ARRANGE
