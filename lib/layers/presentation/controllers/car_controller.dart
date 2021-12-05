d60d35d8c907e64e18e14032eb3fc46162f30de2
Pszione <oused.games@gmail.com>
2021-12-04 23:10:22 -0300

    [07] CLEAN ARCHITECTURE: PRESENTATION LAYER | Controllers

---
+++ lib/layers/presentation/controllers/car_controller.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_clean_architecture/layers/domain/entities/car_entity.dart';
+import 'package:flutter_clean_architecture/layers/domain/usecases/get_cars_by_color/get_cars_by_color_usecase.dart';
+import 'package:flutter_clean_architecture/layers/domain/usecases/save_favorite_car/save_favorite_car_usecase.dart';
+
+class CarController {
+  // Also, you can add logs here, firebase tags etc
+  CarController(
+    this._getCarsByColorUseCase,
+    this._saveFavoriteCarUseCase,
+  );
+  // Usecases contracts
+  final GetCarsByColorUseCase _getCarsByColorUseCase;
+  final SaveFavoriteCarUseCase _saveFavoriteCarUseCase;
+
+  void fetchCarsByColor(String color) {
+    var entity = _getCarsByColorUseCase(color);
+  }
+
+  void saveFavoriteCar(CarEntity car) async {
+    var result = await _saveFavoriteCarUseCase(car);
+  }
+}
