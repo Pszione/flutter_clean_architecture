5c915cbcd6cc5af19d89ef3f125bd9770345a92d
Pszione <oused.games@gmail.com>
2021-12-04 23:21:14 -0300

    [07] CLEAN ARCHITECTURE: PRESENTATION LAYER | UI - Screens part I

---
+++ lib/layers/presentation/ui/pages/car_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_clean_architecture/layers/data/datasources/local/get_cars_by_color_local_datasource_imp.dart';
+import 'package:flutter_clean_architecture/layers/data/repositories/get_cars_by_color_repository_imp.dart';
+import 'package:flutter_clean_architecture/layers/data/repositories/save_favorite_car_repository_imp.dart';
+import 'package:flutter_clean_architecture/layers/domain/usecases/get_cars_by_color/get_cars_by_color_usecase_imp.dart';
+import 'package:flutter_clean_architecture/layers/domain/usecases/save_favorite_car/save_favorite_car_usecase_imp.dart';
+import 'package:flutter_clean_architecture/layers/presentation/controllers/car_controller.dart';
+
+class CarScreen extends StatelessWidget {
+  CarScreen({Key? key}) : super(key: key);
+
+  CarController carController = CarController(
+    GetCarsByColorUseCaseImp(
+      GetCarsByColorRepositoryImp(
+        GetCarsByColorLocalDataSourceImp(),
+      ),
+    ),
+    SaveFavoriteCarUseCaseImp(
+      SaveFavoriteCarRepositoryImp(),
+    ),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(24),
+        child: Text(''), // TODO
+      ),
+    );
+  }
+}
